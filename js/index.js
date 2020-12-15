ef0c39a366186fb2a77b6c805fe8f1fd42c638ad
ArKhosrojerdi <khosrojerdi.alireza@gmail.com>
2020-12-15 20:19:35 +0330

    Doctors page completed

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function openCitiesList() {
-    var citiesDisplay = document.getElementById("cities").style.display;
-    console.log(citiesDisplay)
-    if (citiesDisplay === "" || citiesDisplay === "none")
-        document.getElementById("cities").style.display = "block";
-    else {
-        document.getElementById("cities").style.display = "none";
-    }
-}
